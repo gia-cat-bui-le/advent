7719f66914f63723d4de4885aaeb44d14d8be8e0
gia-cat-bui-le <124648064+gia-cat-bui-le@users.noreply.github.com>
2024-08-14 10:38:39 +0700

    add solution part 1 day 10 year 2023

--- 2023/10/1.py
+++ 2023/10/1.py
@@ -22,15 +22,21 @@ def get_starting_point(find_map):
             return find_map.index(s), s.index("S")
 
 
-def get_related_pos(stand_pos, pipe_pos):
-    return pipe_pos[0] - stand_pos[0], pipe_pos[1] - stand_pos[1]
+def get_related_pos(stand_pos, next_pos):
+    return next_pos[0] - stand_pos[0], next_pos[1] - stand_pos[1]
+
+
+def wait():
+    input("Press to continue")
 
 
 def find_furthest_pos(input_map, cur_point):
     height, width = len(input_map), len(input_map[0])
-    passed_map = [[-1] * width] * height
+    passed_map = [[-1] * width for _ in range(height)]
 
     passed_map[cur_point[0]][cur_point[1]] = 0
+    # print(passed_map)
+
     passed_pos = [cur_point]
 
     furthest_pos = 0
@@ -41,14 +47,16 @@ def find_furthest_pos(input_map, cur_point):
 
         cur_point = passed_map[cur_x][cur_y]
         cur_pipe = input_map[cur_x][cur_y]
+        empty_str = ''
+
+        print(f'cur pos:\n {input_map[cur_x - 1][cur_y] if cur_x >=1 else empty_str}\n{input_map[cur_x][cur_y - 1] if cur_y > 0 else empty_str}{input_map[cur_x][cur_y]}{input_map[cur_x][cur_y + 1] if cur_y + 1 < width else empty_str}\n {input_map[cur_x + 1][cur_y] if cur_x + 1 < height else empty_str}')
 
         for t_x, t_y in directions_dictionary[cur_pipe]:
             n_x, n_y = cur_x + t_x, cur_y + t_y
-            related_pos = get_related_pos((cur_x, cur_y), (n_x, n_y))
+            related_pos = (-t_x, -t_y)
             next_pipe = input_map[n_x][n_y]
             next_dir = directions_dictionary[next_pipe]
-            print(f'related pos: {related_pos}, next dir: {next_dir}, next pos: {(n_x, n_y)}, passed map: {passed_map[n_x][n_y]}')
-            print(f'{not (related_pos in next_dir)}, {passed_map[n_x][n_y] != -1}')
+            print(f'related pos: {related_pos}, next dir: {next_dir}')
             if not (related_pos in next_dir) or passed_map[n_x][n_y] != -1:
                 continue
             print(f'cur_pos: {(cur_x, cur_y)}, cur pos: {cur_point}, next pos: {(n_x, n_y)}, next pipe: {next_pipe}, '
@@ -56,8 +64,8 @@ def find_furthest_pos(input_map, cur_point):
             passed_map[n_x][n_y] = cur_point + 1
             passed_pos.append((n_x, n_y))
             furthest_pos = max(furthest_pos, cur_point + 1)
-
-        print(f'new passed map: {passed_map}')
+            # wait()
+        # print(f'new passed map: {passed_map}')
 
     return furthest_pos
 
